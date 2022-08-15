625c71cceb9d62bf73dec403843841fb61cd4869
cheesecakeassassin <santasebastian@yahoo.com>
2022-08-15 12:17:52 -0400

    Added goodbye button, frame around picture, and added github name

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
@@ -10,10 +9,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.helloButton)
-        button.setOnClickListener {
-            Log.v("Hello world", "Button clicked!")
-            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+
+        // Putting buttons into variables
+        val helloButton = findViewById<Button>(R.id.helloButton)
+        val goodbyeButton = findViewById<Button>(R.id.goodbyeButton)
+
+        // Adding click listeners/handlers to both buttons
+        helloButton.setOnClickListener {
+            Toast.makeText(this, R.string.toast_greeting, Toast.LENGTH_SHORT).show()
+        }
+
+        goodbyeButton.setOnClickListener {
+            Toast.makeText(this, R.string.toast_farewell, Toast.LENGTH_SHORT).show()
         }
     }
 }
